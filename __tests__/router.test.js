a1b7689900ecf80b887288ee8f0b1ef1c324b25e
Long Tang <kyle3tang@gmail.com>
2021-05-23 12:32:37 +0800

    part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,19 @@
 /**
  * @jest-environment jsdom
  */
+import { pushToHistory } from '../scripts/router.js';
+test('Test settings', () => {
+    expect(pushToHistory('settings')).toHaveProperty('state', { page: 'settings' });
+});
 
+test('Test entry and entry number', () => {
+    expect(pushToHistory('entry', 2)).toHaveProperty('state', { page: `entry2` });
+});
+
+test('Default', () => {
+    expect(pushToHistory()).toHaveProperty('state', {});
+});
+
+test('Length', () => {
+    expect(pushToHistory().length).toBe(5);
+});
